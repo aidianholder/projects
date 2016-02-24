007bca223aba092ba7d296d643029bf280850ab6
Aidian Holder <aidianholder@gmail.com>
2016-02-23 21:48:34 -0800

    models built
    Initial models built

--- projects/inspections/models.py
+++ projects/inspections/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+
+@python_2_unicode_compatible
+class Facility(models.Model):
+    name = models.CharField(max_length=100)
+    facility_type = models.CharField(max_length=30, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=25, blank=True)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.city)
+
+    class Meta:
+        unique_together = ("name", "type", "address", "city")
+
+@python_2_unicode_compatible
+class Inspections(models.Model):
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    inspection_type = models.CharField(max_length=50)
+    inspection_date = models.DateField()
+    critical_points = models.IntegerField()
+    total_points = models.IntegerField()
+    inspection_details = models.TextField()
+
+    def __str__(self):
+        return "%s %s %s" % (self.facility, self.inspection_date, self.inspection_type)
+
+
+
--- projects/inspections/urls.py
+++ projects/inspections/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatters = [
     url(r'^$', views.index, name='index'),
-    url(r'^city/(?P<city>\[a-zA-Z]+)/', views.city, name="city")
+    url(r'^city/(?P<city>\[a-zA-Z]+)/', views.city, name="city"),
     url(r'^facility/(?P<city>\D+)/(?P<facility>/[\w\s]+)/$', views.facility, ),
     url(r'^inspection/(?P<city>\D+)/(?P<facility>/[\w\s]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.inspection_detail)
 ]
