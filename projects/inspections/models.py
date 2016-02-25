208213544e598d4654e9a4c3b928516323151adc
Aidian Holder <aidianholder@gmail.com>
2016-02-25 13:51:29 -0800

    fixed name field for Facility model

--- projects/inspections/models.py
+++ projects/inspections/models.py
@@ -7,16 +7,16 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Facility(models.Model):
-    name = models.CharField(max_length=100)
+    facility_name = models.CharField(max_length=100)
     facility_type = models.CharField(max_length=30, blank=True)
     address = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=25, blank=True)
 
     def __str__(self):
-        return "%s %s" % (self.name, self.city)
+        return "%s %s" % (self.facility_name, self.city)
 
     class Meta:
-        unique_together = ("name", "facility_type", "address", "city")
+        unique_together = ("facility_name", "facility_type", "address", "city")
 
 @python_2_unicode_compatible
 class Inspections(models.Model):
@@ -29,6 +29,12 @@ class Inspections(models.Model):
 
     def __str__(self):
         return "%s %s %s" % (self.facility, self.inspection_date, self.inspection_type)
+
+    def passed_or_failed(self):
+        if int(self.total_points) > 40 or int(self.critical_points) > 35:
+            return 'failed'
+        else:
+            return 'passed'
 
 
 
