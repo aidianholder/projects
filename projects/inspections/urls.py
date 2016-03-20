0381c3058c2433b1ef9a6907618d6b3f5ec43cb1
Aidian Holder <aidianholder@gmail.com>
2016-03-19 20:28:08 -0700

    changes to models and views

--- projects/inspections/urls.py
+++ projects/inspections/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^city/(?P<city>\D+)/$', views.city, name="city"),
-    url(r'^facility/(?P<facility>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/$', views.facility, name='facility_detail'),
-    url(r'^inspection/(?P<city>D+)/(?P<facility>/[\w\s]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.inspection_detail, name='inspection_detail'),
+    url(r'^city/(?P<lookup_city>\D+)/$', views.city, name="city"),
+    url(r'^facility/(?P<facility_uuid>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/$', views.facility, name='facility_detail'),
+    url(r'^inspection/(?P<id>\d+)/$', views.inspection_detail, name='inspection_detail'),
 ]
 
 
--- projects/inspections/views.py
+++ projects/inspections/views.py
@@ -12,18 +12,21 @@ def index(request):
 
 def city(request, lookup_city):
     city_facilities = Facility.objects.filter(city=lookup_city)
-    context = {'facilities': city_facilities, 'city': city}
+    context = {'facilities': city_facilities, 'city': lookup_city}
     return render(request, 'inspections/city.html', context)
 
 def facility(request, facility_uuid):
     restaurant = Facility.objects.get(guid=facility_uuid)
     all_inspections = restaurant.inspections_set.all()
     pass_fail_stats = restaurant.pass_fail_total()
+    number_passed = pass_fail_stats['passed']
+    number_failed = pass_fail_stats['failed']
+    percent_passed = pass_fail_stats['passed_percent']
     latest = restaurant.latest_inspection()
-    context = {'facility':restaurant,'inspections':all_inspections,'stats':pass_fail_stats,'latest':latest}
+    context = {'facility':restaurant,'inspections':all_inspections, 'passed':number_passed, 'failed': number_failed, 'percent_passed': percent_passed,'latest':latest}
     return render(request, 'inspections/facility_detail.html', context)
 
-def inspection_detail(request, inspection_identifier):
-    details = Inspections.objects.get(id=inspection_identifier)
+def inspection_detail(request, id):
+    details = Inspections.objects.get(id=id)
     context = {'type':details.inspection_type, 'date': details.inspection_date, 'critical': details.critical_points, 'total': details.total_points, 'notes': details.inspection_details}
     return render(request, 'inspections/inspections_detail.html', context)
