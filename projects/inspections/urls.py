734a90b49f47cc530e787376397e8392ed226c19
Aidian Holder <aidianholder@gmail.com>
2016-04-09 15:49:09 -0700

    JSON architecture changed

--- projects/inspections/urls.py
+++ projects/inspections/urls.py
@@ -4,8 +4,17 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^city/(?P<lookup_city>\D+)/$', views.city, name="city"),
+
     url(r'^facility/(?P<facility_uuid>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/$', views.facility, name='facility_detail'),
     url(r'^inspection/(?P<id>\d+)/$', views.inspection_detail, name='inspection_detail'),
+]
+
+
+
+urlpatterns += [
+    url(r'^json/all/$', views.alljsonp, name="alljsonp"),
+    url(r'^json/city/(?P<l_city>\D+)/$', views.cityjsonp, name="cityjsonp"),
+    url(r'^json/facility/(?P<facility_uuid>[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})/$', views.facilityjsonp, name="facilityjsonp")
 ]
 
 
--- projects/inspections/views.py
+++ projects/inspections/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
 
+import json
 
 from .models import Facility
 from .models import Inspections
 
-# Create your views here.
-
 def index(request):
     return HttpResponse("index page will go here index page will go here")
+
 
 def city(request, lookup_city):
     city_facilities = Facility.objects.filter(city=lookup_city)
     context = {'facilities': city_facilities, 'city': lookup_city}
     return render(request, 'inspections/city.html', context)
+
 
 def facility(request, facility_uuid):
     restaurant = Facility.objects.get(guid=facility_uuid)
@@ -26,7 +29,77 @@ def facility(request, facility_uuid):
     context = {'facility':restaurant,'inspections':all_inspections, 'passed':number_passed, 'failed': number_failed, 'percent_passed': percent_passed,'latest':latest}
     return render(request, 'inspections/facility_detail.html', context)
 
+
 def inspection_detail(request, id):
     details = Inspections.objects.get(id=id)
     context = {'type':details.inspection_type, 'date': details.inspection_date, 'critical': details.critical_points, 'total': details.total_points, 'notes': details.inspection_details}
     return render(request, 'inspections/inspections_detail.html', context)
+
+
+def cityjsonp(request, l_city):
+    city_facilities = Facility.objects.filter(city=l_city)
+    facilities_data = []
+    for establishment in city_facilities:
+        restaurant = dict()
+        restaurant["name"] = establishment.facility_name
+        restaurant["type"] = establishment.facility_type
+        restaurant["address"] = establishment.address
+        restaurant["city"] = establishment.city
+        restaurant["id"] = str(establishment.guid)
+        restaurant["latest"] = establishment.latest_inspection().upper()
+        facilities_data.append(restaurant)
+    json_data = json.dumps(facilities_data)
+    data = '%s(%s)' % (request.GET.get('callback'), json_data)
+    return HttpResponse(data, "text/javascript")
+
+
+def alljsonp(request):
+    facilities = Facility.objects.all()
+    facilities_data = []
+    for establishment in facilities:
+        restaurant = dict()
+        restaurant["name"] = establishment.facility_name
+        restaurant["type"] = establishment.facility_type
+        restaurant["address"] = establishment.address
+        restaurant["city"] = establishment.city
+        restaurant["id"] = str(establishment.guid)
+        restaurant["latest"] = establishment.latest_inspection().upper()
+        facilities_data.append(restaurant)
+    json_data = json.dumps(facilities_data)
+    data = '%s(%s)' % (request.GET.get('callback'), json_data)
+    return HttpResponse(data, "text/javascript")
+
+
+def facilityjsonp(request, facility_uuid):
+    facility = Facility.objects.get(guid=facility_uuid)
+    inspections_performed = Inspections.objects.filter(facility_guid_id = facility_uuid)
+    facility_details = dict()
+    facility_details["name"] = facility.facility_name
+    facility_details["type"] = facility.facility_type
+    facility_details["address"] = facility.address
+    facility_details["city"] = facility.city
+    pfTotals = facility.pass_fail_total()
+    facility_details["pass"] = int(pfTotals['passed'])
+    facility_details["fail"] = int(pfTotals['failed'])
+    inspections = []
+    for inspection in inspections_performed:
+        record = dict()
+        record["date"] = inspection.inspection_date.strftime("%x")
+        record["type"] = inspection.inspection_type
+        record["critical"] = inspection.critical_points
+        record["total"] = inspection.total_points
+        record["notes"] = inspection.inspection_details
+        record["pf"] = inspection.passed_or_failed()
+        inspections.append(record)
+    data_object = {"facility": facility_details, "inspections": inspections}
+    json_data = json.dumps(data_object)
+    json_data.replace("\n", " ")
+    data = '%s(%s)' % (request.GET.get('callback'), json_data)
+    return HttpResponse(data, "text/javascript")
+
+    #inspection_record["inspections"] = []
+
+#json_data = serializers.serialize('json', city_facilities, fields=('facility_name', 'facility_type', 'city', 'address', 'guid'))
+    #callback = 'myFunc'
+
+    #return JsonResponse('%s(%s)' % (request.GET.get('callback'), facilities_data), safe=False)
